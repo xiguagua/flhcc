6a4eb6b3d5b7f3fb80439c99899303a0ac5b8827
xiguagua <rootxigua@gmail.com>
2024-08-06 04:26:30 +0000

    feat: deploy to github

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
+  site: "https://www.flhcctech.com",
+  // base: "flhcc"
   integrations: [mdx(), sitemap(), tailwind()],
 });
