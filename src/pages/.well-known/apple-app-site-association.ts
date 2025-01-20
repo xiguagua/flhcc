d9f55f4a5444fb8009021028a50c51e077cacf5c
xiguagua <rootxigua@live.com>
2025-01-20 21:25:45 +0800

    feat: rule-providers page

--- src/pages/.well-known/apple-app-site-association.ts
+++ src/pages/.well-known/apple-app-site-association.ts
@@ -1,8 +1,8 @@
 import content from './_content.json'
 
-export async function GET({params, request}) {
-    return new Response(
-      JSON.stringify(content)
-    )
-  }
+export async function GET({ params, request }) {
+  return new Response(
+    JSON.stringify(content)
+  )
+}
 
