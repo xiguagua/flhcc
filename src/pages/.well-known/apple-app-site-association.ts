882e73670d8b25299872d8f0fe279cf26cbcdba3
xiguagua <rootxigua@live.com>
2025-01-21 15:39:07 +0800

    new: applinks

--- src/pages/.well-known/apple-app-site-association.ts
+++ src/pages/.well-known/apple-app-site-association.ts
@@ -1,8 +1,8 @@
-import content from './_content.json'
+import applinks from './_applinks.json'
 
 export async function GET({ params, request }) {
   return new Response(
-    JSON.stringify(content)
+    JSON.stringify(applinks)
   )
 }
 
