2022914ec9326600f53e92c64efe9f9c5af6110e
xiguagua <rootxigua@live.com>
2025-01-23 21:38:29 +0800

    feat: import _items.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
+
 export default defineConfig({
   site: "https://www.flhcctech.com",
   // base: "flhcc"
