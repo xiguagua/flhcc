93071df30eb1c352dde758fbd9ee047b6192abd1
xiguagua <rootxigua@live.com>
2025-02-25 11:43:56 +0800

    feat: upgrade tailwindcss 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
+// import tailwind from "@astrojs/tailwind";
+
+
+import tailwindcss from "@tailwindcss/vite";
 
 
 export default defineConfig({
   site: "https://www.flhcctech.com",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
